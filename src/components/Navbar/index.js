450ba2545f011d54f51b0ab883c1b4b03bce9d8e
Seth Zyg <zygster11@gmail.com>
2021-03-25 12:26:04 -0500

    Should have fixed the navbar brand link giving a 404 error on github pages.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
-import "./style.css"
+import "./style.css";
 
 const Navbar = (props) => {
 
-  const handlePageChange = () => {
-    window.location.href = "/"
-  }
 
   return (
     <nav className="navbar navbar-expand-lg navbar-collapse-md navbar-collapse-sm navbar-dark bg-dark Navbar">
-      <span onClick={handlePageChange} className="navbar-brand mb-1" id="Span">
-        Seth Zygarlicke
-      </span>
-
+      <Link id="routerLink" to="/">
+        <span
+          className="navbar-brand mb-1"
+          id="Span"
+        >
+          Seth Zygarlicke
+        </span>
+      </Link>
       <button
         className="navbar-toggler"
         type="button"
@@ -30,7 +31,7 @@ const Navbar = (props) => {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="nav navbar-nav navbar-right ms-auto">
           <li className="nav-item">
-            <Link className="nav-link" id="navLink"  to="/contact">
+            <Link className="nav-link" id="navLink" to="/contact">
               Contact
             </Link>
           </li>
